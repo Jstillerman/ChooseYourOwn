4a022e9912d3913cf27a693ced6a999f27c95f8c
Jason Stillerman <jason.t.stillerman@gmail.com>
2017-12-06 18:22:04 -0500

    fixed weird issues

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env']
-          }
-        }
-      }
-    ]
+	    loaders: [
+		        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+	      ]
   }
 };
